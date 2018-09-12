c130fb274a825b8f9b7357495a651c8271e356d6
napo <elnapol@gmail.com>
2018-09-12 07:06:35 -0300

    fire base primer duncionamiento

---
+++ TimeToWater/CURL.php
@@ -0,0 +1,21 @@
+<?php
+//https://time2water-2b8e6.firebaseio.com/
+$data = '{"concepto":"Curso de PHP", "subroral":"200","ID":1}';
+$url = "https://time2water-2b8e6.firebaseio.com/presupuestos.json";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content_Type: text/plain'));
+
+$response = curl_exec($ch);
+
+if(curl_errno($ch)){
+	echo 'Error:'.curl_errno($ch);
+}else{
+	echo "Ya se insertó";
+}
+curl_close($ch);
+?>
---
+++ TimeToWater/recepcion/conexion_firebase.php
@@ -0,0 +1,123 @@
+<?php   
+	 //    include("conexion.php");
+		// $con = new conexion();
+    // Si se recibe Datos con el Método GET, los procesamos
+   if (isset($_GET['T']) && isset($_GET['H']) && isset($_GET['H2'])){
+        $temp = $_GET['T'];
+        $hum = $_GET['H'];
+        $humti = $_GET['H2'];
+        // $con->insertarDatos($temp,$hum,$humti);
+    }
+    // $estado = $con->cambiarRegador();
+    // if($estado[0] == "off"){
+    //     echo "Estado:0";
+    // }else{
+    //     echo "Estado:1";
+    // }
+?>
+<script>
+    
+window.onload = inicializar;
+var formConvalidaciones;
+var refConvalidaciones;
+var tbodyTablaConvalidaciones;
+var CREATE = "Anadir Convalidacion";
+var UPDATE = "Modificar Convalidacion";
+var modo = CREATE;
+var refConvalidacionAEditar;
+
+function inicializar(){
+    formConvalidaciones = document.getElementById("form-convalidaciones");
+    formConvalidaciones.addEventListener("submit", enviarConvalidacionAFirebase, false)
+
+    tbodyTablaConvalidaciones = document.getElementById("tbody-tabla-convalidaciones");
+
+    refConvalidaciones = firebase.database().ref().child("convalidaciones");
+
+    mostrarConvalidacionesDeFirebase();
+} 
+
+function mostrarConvalidacionesDeFirebase(){
+    refConvalidaciones.on("value", function(snap){
+        var datos = snap.val();
+        var filasAmostrar = "";
+        for(var key in datos){
+            filasAmostrar += "<tr>" +
+                                "<td>" + datos[key].cicloAConvalidar + "</td>" +
+                                "<td>" + datos[key].moduloAConvalidar + "</td>" +
+                                "<td>" + datos[key].cicloAportado + "</td>" +
+                                "<td>" + datos[key].moduloAportado + "</td>" +
+                                '<td>' +
+                                    '<button class="btn btn-default editar" data-convalidacion="' + key + '">' +
+                                        '<span class="glyphicon glyphicon-pencil"></span>' +
+                                    '</button>' +
+                                '</td>' +
+                                '<td>' +
+                                    '<button class="btn btn-danger borrar" data-convalidacion="' + key + '">' +
+                                        '<span class="glyphicon glyphicon-trash"></span>' +
+                                    '</button>' +
+                                '</td>' +
+                             "</tr>";
+        }
+        tbodyTablaConvalidaciones.innerHTML = filasAmostrar;
+        if(filasAmostrar != ""){
+            var elementosEditables = document.getElementsByClassName("editar");
+            for(var i = 0; i < elementosEditables.length; i++){
+                elementosEditables[i].addEventListener("click", editarConvalidacionDeFirebase, false);
+            }
+        
+            var elementosBorrables = document.getElementsByClassName("borrar");
+            for(var i = 0; i < elementosBorrables.length; i++){
+                elementosBorrables[i].addEventListener("click", borrarConvalidacionDeFirebase, false);
+            }
+        }
+    });
+    
+
+}
+
+function editarConvalidacionDeFirebase(){
+    var keyDeConvalidacionAEditar = this.getAttribute("data-convalidacion");
+    refConvalidacionAEditar = refConvalidaciones.child(keyDeConvalidacionAEditar);
+    refConvalidacionAEditar.once("value", function(snap){
+        var datos = snap.val(); 
+        document.getElementById("modulo-a-convalidar").value = datos.moduloAConvalidar;
+        document.getElementById("ciclo-a-convalidar").value = datos.cicloAConvalidar;
+        document.getElementById("modulo-aportado").value = datos.moduloAportado;
+        document.getElementById("ciclo-aportado").value = datos.cicloAportado;
+    });
+    document.getElementById("boton-enviar-convalidacion").value = UPDATE;
+    modo = UPDATE;
+}
+
+function borrarConvalidacionDeFirebase(){
+    var keyDeConvalidacionABorrar = this.getAttribute("data-convalidacion");
+    var refConvalidacionABorrar = refConvalidaciones.child(keyDeConvalidacionABorrar);
+    refConvalidacionABorrar.remove();
+}
+
+function enviarConvalidacionAFirebase(){
+    event.preventDefault();
+    switch(modo){
+        case CREATE:
+            refConvalidaciones.push({
+                moduloAConvalidar: event.target.moduloAConvalidar.value,
+                cicloAConvalidar: event.target.cicloAConvalidar.value,
+                moduloAportado: event.target.moduloAportado.value,
+                cicloAportado: event.target.cicloAportado.value
+            });
+        break;
+        case UPDATE:
+            refConvalidacionAEditar.update({
+                moduloAConvalidar: event.target.moduloAConvalidar.value,
+                cicloAConvalidar: event.target.cicloAConvalidar.value,
+                moduloAportado: event.target.moduloAportado.value,
+                cicloAportado: event.target.cicloAportado.value
+            });
+            modo = CREATE;
+            document.getElementById("boton-enviar-convalidacion").value = CREATE;
+        break;
+    }
+    formConvalidaciones.reset();
+}
+</script>
